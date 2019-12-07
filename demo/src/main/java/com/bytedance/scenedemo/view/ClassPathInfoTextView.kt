cc6be223f44ce472d5158d5b8a3fe17d21429499
qii <qiibeta@gmail.com>
2019-12-07 19:52:26 +0800

    chroe: fix demo compile issue

--- demo/src/main/java/com/bytedance/scenedemo/view/ClassPathInfoTextView.kt
+++ demo/src/main/java/com/bytedance/scenedemo/view/ClassPathInfoTextView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.TextView
 import com.bytedance.scene.ktx.getScene
+import com.bytedance.scene.ktx.requireScene
 
 
 class ClassPathInfoTextView @JvmOverloads constructor(
@@ -11,6 +12,6 @@ class ClassPathInfoTextView @JvmOverloads constructor(
 ) : TextView(context, attrs, defStyleAttr) {
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
-        this.text = getScene().javaClass.name
+        this.text = requireScene().javaClass.name
     }
 }
