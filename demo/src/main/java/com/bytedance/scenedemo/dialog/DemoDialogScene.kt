24f1a96f814f851f998fe595ed70ba00fd3e5649
qii <qiibeta@gmail.com>
2020-12-22 17:58:25 +0800

    refactor: refactor demo to kotlin code

--- demo/src/main/java/com/bytedance/scenedemo/dialog/DemoDialogScene.kt
+++ demo/src/main/java/com/bytedance/scenedemo/dialog/DemoDialogScene.kt
@@ -1,42 +1,28 @@
-package com.bytedance.scenedemo.dialog;
+package com.bytedance.scenedemo.dialog
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-
-import com.bytedance.scene.Scene;
-import com.bytedance.scenedemo.utility.ColorUtil;
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.bytedance.scene.Scene
+import com.bytedance.scenedemo.utility.ColorUtil
 
 /**
  * Created by JiangQi on 8/8/18.
  */
-public class DemoDialogScene extends Scene {
-    @NonNull
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FrameLayout layout = new FrameLayout(requireSceneContext());
-
-        TextView textView = new TextView(getActivity());
-        textView.setText(getNavigationScene().getStackHistory());
-        textView.setBackgroundColor(ColorUtil.getMaterialColor(getResources(), 1));
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(600, 600);
-        layoutParams.gravity = Gravity.CENTER;
-
-        layout.addView(textView, layoutParams);
-
-        layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requireNavigationScene().pop();
-            }
-        });
-
-        return layout;
+class DemoDialogScene : Scene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
+        val layout = FrameLayout(requireSceneContext())
+        val textView = TextView(activity)
+        textView.text = requireNavigationScene().stackHistory
+        textView.setBackgroundColor(ColorUtil.getMaterialColor(resources, 1))
+        val layoutParams = FrameLayout.LayoutParams(600, 600)
+        layoutParams.gravity = Gravity.CENTER
+        layout.addView(textView, layoutParams)
+        layout.setOnClickListener { requireNavigationScene().pop() }
+        return layout
     }
 }
--- demo/src/main/java/com/bytedance/scenedemo/dialog/DemoDialogWithDimScene.kt
+++ demo/src/main/java/com/bytedance/scenedemo/dialog/DemoDialogWithDimScene.kt
@@ -1,44 +1,30 @@
-package com.bytedance.scenedemo.dialog;
+package com.bytedance.scenedemo.dialog
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-
-import com.bytedance.scene.Scene;
-import com.bytedance.scenedemo.utility.ColorUtil;
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.TextView
+import com.bytedance.scene.Scene
+import com.bytedance.scenedemo.utility.ColorUtil
 
 /**
  * Created by JiangQi on 8/9/18.
  */
-public class DemoDialogWithDimScene extends Scene {
-    @NonNull
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FrameLayout layout = new FrameLayout(requireSceneContext());
-        layout.setBackgroundColor(Color.parseColor("#99000000"));
-
-        TextView textView = new TextView(getActivity());
-        textView.setText(getNavigationScene().getStackHistory());
-        textView.setBackgroundColor(ColorUtil.getMaterialColor(getResources(), 1));
-        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(600, 600);
-        layoutParams.gravity = Gravity.CENTER;
-
-        layout.addView(textView, layoutParams);
-
-        layout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                requireNavigationScene().pop();
-            }
-        });
-
-        return layout;
+class DemoDialogWithDimScene : Scene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): View {
+        val layout = FrameLayout(requireSceneContext())
+        layout.setBackgroundColor(Color.parseColor("#99000000"))
+        val textView = TextView(activity)
+        textView.text = requireNavigationScene().stackHistory
+        textView.setBackgroundColor(ColorUtil.getMaterialColor(resources, 1))
+        val layoutParams = FrameLayout.LayoutParams(600, 600)
+        layoutParams.gravity = Gravity.CENTER
+        layout.addView(textView, layoutParams)
+        layout.setOnClickListener { requireNavigationScene().pop() }
+        return layout
     }
 }
