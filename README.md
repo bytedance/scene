318e28b65473acce4da77e34aca4d44ad5d161df
qii <qiibeta@gmail.com>
2020-12-29 18:24:20 +0800

    feature: deep links

---
+++ README.md
@@ -0,0 +1,43 @@
+AndroidManifest define scheme
+```xml
+            <intent-filter android:label="@string/filter_view_http_gizmos">
+                <action android:name="android.intent.action.VIEW" />
+
+                <category android:name="android.intent.category.DEFAULT" />
+                <category android:name="android.intent.category.BROWSABLE" />
+                <!-- Accepts URIs that begin with "http://www.example.com/gizmos” -->
+                <data
+                    android:scheme="http"
+                    android:host="www.example.com"
+                    android:pathPrefix="/gizmos" />
+                <!-- note that the leading "/" is required for pathPrefix-->
+            </intent-filter>
+            <intent-filter android:label="@string/filter_view_example_gizmos">
+                <action android:name="android.intent.action.VIEW" />
+
+                <category android:name="android.intent.category.DEFAULT" />
+                <category android:name="android.intent.category.BROWSABLE" />
+                <!-- Accepts URIs that begin with "example://gizmos” -->
+                <data
+                    android:scheme="example"
+                    android:host="gizmos" />
+            </intent-filter>
+```
+
+SceneUrl
+```kotlin
+@SceneUrl("/test2")
+class BScene : GroupScene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): ViewGroup {
+        return FrameLayout(requireSceneContext()).apply {
+            setBackgroundColor(Color.YELLOW)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
+```
+
+adb shell am start -W -a android.intent.action.VIEW -d http://www.example.com/gizmos/test2 com.bytedance.scene.deeplinks
---
+++ app/src/main/java/com/bytedance/scene/deeplinks/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.bytedance.scene.deeplinks
+
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.bytedance.scene.NavigationSceneUtility
+import com.bytedance.scene.Scene
+import com.bytedance.scene.SceneDelegate
+import com.bytedance.scene.group.GroupScene
+import com.bytedance.scenerouter.annotation.SceneUrl
+import com.bytedance.scenerouter.core.OpenCallback
+import com.bytedance.scenerouter.core.SceneNotFoundException
+import com.bytedance.scenerouter.core.SceneRouter
+import com.bytedance.scenerouter.core.SceneRouters
+
+//TODO support url query argument
+class MainActivity : AppCompatActivity() {
+    private lateinit var mDelegate: SceneDelegate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.decorView.systemUiVisibility = (window.decorView.systemUiVisibility
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+        }
+        this.mDelegate = NavigationSceneUtility.setupWithActivity(this, getHomeSceneClass())
+            .supportRestore(false).build()
+
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+        handleSchema(action, data)
+    }
+
+    private fun getHomeSceneClass(): Class<out Scene> {
+        return MainScene::class.java
+    }
+
+    override fun onBackPressed() {
+        if (!this.mDelegate.onBackPressed()) {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        val action: String? = intent?.action
+        val data: Uri? = intent?.data
+        handleSchema(action, data)
+    }
+
+    private fun handleSchema(action: String?, data: Uri?) {
+        if (data == null) {
+            return
+        }
+        val navigationScene = this.mDelegate.navigationScene ?: return
+        val uri = data.path!!.removePrefix("/gizmos")
+
+        if (navigationScene.lifecycle.currentState.isAtLeast(Lifecycle.State.CREATED)) {
+            SceneRouters.of(navigationScene).url(uri).argument("uri", uri).open()
+        } else {
+            navigationScene.lifecycle.addObserver(object : LifecycleObserver {
+                @OnLifecycleEvent(Lifecycle.Event.ON_START)
+                fun onCreate() {
+                    SceneRouters.of(navigationScene).url(uri).argument("uri", uri).open()
+                }
+            })
+        }
+    }
+}
+
+class MainScene : GroupScene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): ViewGroup {
+        return FrameLayout(requireSceneContext())
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
+
+//TODO url match
+@SceneUrl("/test1")
+class AScene : GroupScene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): ViewGroup {
+        return FrameLayout(requireSceneContext()).apply {
+            setBackgroundColor(Color.RED)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
+
+@SceneUrl("/test2")
+class BScene : GroupScene() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle?): ViewGroup {
+        return FrameLayout(requireSceneContext()).apply {
+            setBackgroundColor(Color.YELLOW)
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
