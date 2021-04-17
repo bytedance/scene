a7e0df6acd43108fef9e489c97dd210604823599
qii <qiibeta@gmail.com>
2021-04-17 18:34:44 +0800

    refactor: prepare to update to Robolectric 4.5.1

--- library/scene/src/test/java/com/bytedance/scene/MessengerHandlerTests.java
+++ library/scene/src/test/java/com/bytedance/scene/MessengerHandlerTests.java
@@ -7,14 +7,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
+import org.robolectric.annotation.LooperMode;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import static android.os.Looper.getMainLooper;
 import static org.junit.Assert.assertEquals;
+import static org.robolectric.Shadows.shadowOf;
+import static org.robolectric.annotation.LooperMode.Mode.PAUSED;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
+@LooperMode(PAUSED)
 public class MessengerHandlerTests {
     @Test
     public void test() {
@@ -31,6 +35,8 @@ public class MessengerHandlerTests {
         Bundle result = new Bundle();
         result.putString("KEY", "VALUE");
         MessengerHandler.from(intent).sendResult(result);
+        shadowOf(getMainLooper()).idle();
+
         assertEquals("VALUE", atomicReference.get());
     }
 }
