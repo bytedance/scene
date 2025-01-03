b0a9e2582c9f29f8b82f7aa2998a9323188d6205
qii <qiibeta@gmail.com>
2025-01-03 17:39:47 +0800

    Revert "refactor: PopOptions setAnimation add reversePopAnimation parameter in order to AnimationOrAnimatorResourceExecutor to skip reverse animation"

--- library/scene_navigation/src/main/java/com/bytedance/scene/animation/animatorexecutor/AnimationOrAnimatorResourceExecutor.java
+++ library/scene_navigation/src/main/java/com/bytedance/scene/animation/animatorexecutor/AnimationOrAnimatorResourceExecutor.java
@@ -38,15 +38,10 @@ import com.bytedance.scene.utlity.CancellationSignal;
 public final class AnimationOrAnimatorResourceExecutor extends NavigationAnimationExecutor {
     private AnimationOrAnimator mEnterAnimator;
     private AnimationOrAnimator mExitAnimator;
-    private boolean mReversePopAnimation = true;
 
     public AnimationOrAnimatorResourceExecutor(Activity activity, @AnimatorRes @AnimRes int enterResId, @AnimatorRes @AnimRes int exitResId) {
         mEnterAnimator = AnimationOrAnimator.loadAnimation(activity, enterResId);
         mExitAnimator = AnimationOrAnimator.loadAnimation(activity, exitResId);
-    }
-
-    public void setReversePopAnimation(boolean reversePopAnimation){
-        this.mReversePopAnimation = reversePopAnimation;
     }
 
     @Override
@@ -149,15 +144,11 @@ public final class AnimationOrAnimatorResourceExecutor extends NavigationAnimati
             }
         });
 
-        if (this.mReversePopAnimation) {
-            mEnterAnimator.reverse();
-        }
+        mEnterAnimator.reverse();
         mEnterAnimator.addEndAction(animationEndAction);
         mEnterAnimator.start(fromView);
 
-        if (this.mReversePopAnimation) {
-            mExitAnimator.reverse();
-        }
+        mExitAnimator.reverse();
         mExitAnimator.addEndAction(animationEndAction);
         mExitAnimator.start(toView);
         cancellationSignal.setOnCancelListener(new CancellationSignal.OnCancelListener() {
